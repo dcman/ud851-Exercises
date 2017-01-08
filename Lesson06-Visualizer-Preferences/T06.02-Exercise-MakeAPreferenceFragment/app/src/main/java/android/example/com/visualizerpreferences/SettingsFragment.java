27dffe6163d38c93a71c884260bbb9c0dabe35d6
dcman <justin.johnson.90@my.csun.edu>
2017-01-07 17:26:23 -0800

    Added setting fragment

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,15 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by uc on 1/7/17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+}
